5ee86ef17c62d9ef5752b835f9dadd628f2416cf
Vyshnavi Yeldanda <vaishnaviyeldhanda22@gmail.com>
2026-03-07 05:00:09 +0530

    Time: 10 ms (46.27%) | Memory: 11.2 MB (85.36%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        bool cycex = false;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast){
+                cycex = true;
+                break;
+            }
+        }
+
+        if(!cycex){
+            return NULL;
+        }
+
+        slow = head;
+
+        while(slow != fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return slow;
+    }
+};
